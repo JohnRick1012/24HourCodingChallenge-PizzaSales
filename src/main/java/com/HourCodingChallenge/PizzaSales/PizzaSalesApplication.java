31999c7ac30a9546cc0fe15ad5378f52951d9427
JohnRick1012 <exmenosnilomaj@gmail.com>
2023-06-04 12:16:56 +0800

    Initial Commit

---
+++ src/main/java/com/HourCodingChallenge/PizzaSales/PizzaSalesApplication.java
@@ -0,0 +1,13 @@
+package com.HourCodingChallenge.PizzaSales;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaSalesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaSalesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HourCodingChallenge/PizzaSales/PizzaSalesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HourCodingChallenge.PizzaSales;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PizzaSalesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
